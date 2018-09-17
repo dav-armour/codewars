ff571f093d9d0d4a95e43d6925e85907b137c590
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-17 23:36:54 +1000

    refactor roman

--- roman-numerals.rb
+++ roman-numerals.rb
@@ -2,92 +2,39 @@ class RomanNumerals
 
   @value_hash = {
     'I' => 1,
+    'IV' => 4,
     'V' => 5,
+    'IX' => 9,
     'X' => 10,
+    'XL' => 40,
     'L' => 50,
+    'XC' => 90,
     'C' => 100,
+    'CD' => 400,
     'D' => 500,
+    'CM' => 900,
     'M' => 1000
   }
 
   class << self
     def to_roman(number)
+      number = number
       string = ""
-      n = number / 1000
-      if n > 0
-        string += 'M' * n
-        number %= 1000
-      end
-      n = number / 900
-      if n > 0
-        string += 'CM'
-        number %= 900
-      end
-      n = number / 500
-      if n > 0
-        string += 'D'
-        number %= 500
-      end
-      n = number / 400
-      if n > 0
-        string += 'CD'
-        number %= 400
-      end
-      n = number / 100
-      if n > 0
-        string += 'C' * n
-        number %= 100
-      end
-      n = number / 90
-      if n > 0
-        string += 'XC'
-        number %= 90
-      end
-      n = number / 50
-      if n > 0
-        string += 'L'
-        number %= 50
-      end
-      n = number / 40
-      if n > 0
-        string += 'XL'
-        number %= 40
-      end
-      n = number / 10
-      if n > 0
-        string += 'X' * n
-        number %= 10
-      end
-      n = number / 9
-      if n > 0
-        string += 'IX'
-        number %= 9
-      end
-      n = number / 5
-      if n > 0
-        string += 'V'
-        number %= 5
-      end
-      n = number / 4
-      if n > 0
-        string += 'IV'
-        number %= 4
-      end
-      n = number / 1
-      if n > 0
-        string += 'I' * n
+      @value_hash.values.reverse.each do |val|
+        n = number / val
+        if n > 0
+          string += @value_hash.key(val) * n
+          number %= val
+        end
       end
       string
     end
 
-   def from_roman(string)
+    def from_roman(string)
       value = 0
       chars_array = string.chars
       i = 0
-      p chars_array
-      p chars_array.length
       while i < chars_array.length
-        p i
         cur_val = @value_hash[chars_array[i]]
         if i < chars_array.length - 1
           next_val = @value_hash[chars_array[i + 1]]
